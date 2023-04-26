4b16e31f505f4fb2aa695a4f9364df7a76e4547f
binford@thecave <jb.williams016@gmail.com>
2023-04-25 21:15:13 -0500

    messing with fileouput

--- main.go
+++ main.go
@@ -91,17 +91,36 @@ func scanningTCP(netWork string, tcpScan string, port int, doneChannel chan bool
 	conn.Close()
 	checkConn := "[+] " + strconv.Itoa(port) + " connected"
 	// TRY TO DO NEW BUFFER TO WRITE TO FILE
+<<<<<<< HEAD
 	// 	err = ioutil.WriteFile(outFile, []byte(checkConn), 0644)
 	// 	if err != nil {
 	// 		lPf("ERROR: Failed to write to %s", outfile)
 	// 	}
 	// }
 	// lPf("[+] %d connnected", port)
+=======
+	//if outFile != "" {
+	//	outfile, err := os.Create(outFile)
+	//	if err != nil {
+	//		lPf("ERROR: Failed to create %s", outfile)
+	//	}
+	//	defer outfile.Close()
+	//	err = ioutil.WriteFile(outFile, []byte(checkConn), 0644)
+	//	if err != nil {
+	//		lPf("ERROR: Failed to write to %s", outfile)
+	//	}
+	//}
+	//lPf("[+] %d connnected", port)
+>>>>>>> 52ec809 (messing with fileouput)
 	lPf(string(checkConn[:]))
 	doneChannel <- true
 }
 
+<<<<<<< HEAD
 func scanningUDP(netWork string, host2Scan string, port int, doneChannel chan bool) {
+=======
+func scanningUDP(netWork string, host2Scan string, port int, doneChannel chan bool, outFile string) {
+>>>>>>> 52ec809 (messing with fileouput)
 	timeoutLength := 5 * time.Second
 	conn, err := net.DialTimeout(netWork, host2Scan+":"+strconv.Itoa(port), timeoutLength)
 	if err != nil {
@@ -110,6 +129,7 @@ func scanningUDP(netWork string, host2Scan string, port int, doneChannel chan bo
 	}
 	conn.Close()
 	checkConn := "[+] " + strconv.Itoa(port) + " connected"
+<<<<<<< HEAD
 	// if outFile != "" {
 	// 	outfile, err := os.Create(outFile)
 	// 	if err != nil {
@@ -121,6 +141,19 @@ func scanningUDP(netWork string, host2Scan string, port int, doneChannel chan bo
 	// 		lPf("ERROR: Failed to write to %s", outfile)
 	// 	}
 	// }
+=======
+	//if outFile != "" {
+	//	outfile, err := os.Create(outFile)
+	//	if err != nil {
+	//		lPf("ERROR: Failed to create %s", outfile)
+	//	}
+	//	defer outfile.Close()
+	//	err = ioutil.WriteFile(outFile, []byte(checkConn), 0644)
+	//	if err != nil {
+	//		lPf("ERROR: Failed to write to %s", outfile)
+	//	}
+	//}
+>>>>>>> 52ec809 (messing with fileouput)
 	lPf(string(checkConn[:]))
 	doneChannel <- true
 }
